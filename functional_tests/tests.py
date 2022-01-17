5889050274f3746e1a0a687cc3327a11f64b5327
p.leinen <philipp_leinen@web.de>
2022-01-17 18:43:27 +0100

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -42,7 +42,6 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
         
         
-        
     def test_can_start_a_list_for_one_user(self):      
         # Edith has heard about a cool new online to-do app. 
         # She goes to check out its homepage.
@@ -51,8 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-        
+        self.assertIn('To-Do', header_text)        
         
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -70,7 +68,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)       
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         
-        
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -79,15 +76,10 @@ class NewVisitorTest(LiveServerTestCase):
         
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        
-
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')      
         
         # Satisfied, she goes back to sleep
-        
     
-        
-        
         
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith start a new to-do list
@@ -134,5 +126,26 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisfied, they both go back to sleep
         
 
-
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768) # in pixel
         
+        # Sche notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 10
+        )
+        
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(('1: testing'))
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 10
+        )
